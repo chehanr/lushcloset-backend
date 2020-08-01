d6bad47bfc2cc42cff300cebdb6e0bad1f27865c
Chehan Ratnasiri <chehan.rat@gmail.com>
2020-08-01 18:41:44 +1000

    Added retrieve controller and changed create response.

--- src/controllers/listing.js
+++ src/controllers/listing.js
@@ -1,6 +1,7 @@
 const BaseController = require('./base');
 const models = require('../models');
 const googleMapsHelper = require('../helpers/google-maps');
+const googleMapsUtils = require('../utils/google-maps');
 
 class ListingController extends BaseController {
   /**
@@ -79,21 +80,167 @@ class ListingController extends BaseController {
         };
       });
 
+      result.user = await result.listing.getUser();
+
+      const addressComponentsObj = googleMapsUtils.getAddressComponents(
+        geocodingData[0]
+      );
+
+      const approxAddressComponentsObj = googleMapsUtils.getAddressComponents(
+        geocodingData[0],
+        true
+      );
+
       const responsObj = {
+        id: result.listing.id,
         title: result.listing.title,
+        shortDescription: null,
         description: result.listing.description,
         listingType: result.listing.listingType,
-        priceValue: result.listingPrice.value,
-        currencyTypeIso: result.listingPrice.currencyTypeIso,
-        submittedAddress: result.listingAddress.submittedAddress,
-        formattedAddress: result.listingAddress.formattedAddress,
-        addressNote: result.listingAddress.note,
+        price: {
+          value: result.listingPrice.value,
+          currencyTypeIso: result.listingPrice.currencyTypeIso,
+        },
+        user: {
+          id: result.user.id,
+          name: result.user.name,
+        },
+        approximateLocation: {
+          formattedAddress: googleMapsUtils.getFormattedAddress(
+            approxAddressComponentsObj
+          ),
+          addressComponents: approxAddressComponentsObj,
+        },
+        preciseLocation: {
+          submittedAddress: result.listingAddress.submittedAddress,
+          formattedAddress: result.listingAddress.formattedAddress,
+          addressComponents: addressComponentsObj,
+          geographic: {
+            lat:
+              result.listingAddress.googleGeocodingData?.geometry?.location
+                .lat || null,
+            lng:
+              result.listingAddress.googleGeocodingData?.geometry?.location
+                .lng || null,
+          },
+          googlePlaceId:
+            // eslint-disable-next-line camelcase
+            result.listingAddress.googleGeocodingData?.place_id || null,
+          note: result.listingAddress.note || null,
+        },
         status: result.listingStatus.statusType,
         createdAt: result.listing.createdAt,
         updatedAt: result.listing.updatedAt,
       };
 
       return this.created(res, responsObj);
+    } catch (error) {
+      return this.fail(res, error);
+    }
+  }
+
+  /**
+   * Retrieve a listing \
+   * with params `listingId`.
+   */
+  async retrieveListingItem(req, res) {
+    const errorResponseObj = { validation: {} };
+
+    if (req.validated.params?.error) {
+      errorResponseObj.validation.params = req.validated.params.error;
+
+      return this.unprocessableEntity(res, errorResponseObj);
+    }
+
+    try {
+      const listingObj = await models.Listing.findByPk(
+        req.validated.params.value.listingId,
+        {
+          include: [
+            {
+              model: models.User,
+              as: 'user',
+            },
+            {
+              model: models.ListingAddress,
+              as: 'listingAddress',
+            },
+            {
+              model: models.ListingPrice,
+              as: 'listingPrice',
+            },
+            {
+              model: models.ListingStatus,
+              as: 'listingStatus',
+            },
+          ],
+        }
+      );
+
+      if (listingObj) {
+        const geocodingData = listingObj.listingAddress.googleGeocodingData;
+
+        const addressComponentsObj = googleMapsUtils.getAddressComponents(
+          geocodingData
+        );
+
+        const approxAddressComponentsObj = googleMapsUtils.getAddressComponents(
+          geocodingData,
+          true
+        );
+
+        const responseObj = {
+          id: listingObj.id,
+          title: listingObj.title,
+          shortDescription: null,
+          description: listingObj.description,
+          listingType: listingObj.listingType,
+          price: {
+            value: listingObj.listingPrice.value,
+            currencyTypeIso: listingObj.listingPrice.currencyTypeIso,
+          },
+          user: {
+            id: listingObj.user.id,
+            name: listingObj.user.name,
+          },
+          approximateLocation: {
+            formattedAddress: googleMapsUtils.getFormattedAddress(
+              approxAddressComponentsObj
+            ),
+            addressComponents: approxAddressComponentsObj,
+          },
+          preciseLocation: {
+            submittedAddress: listingObj.listingAddress.submittedAddress,
+            formattedAddress: listingObj.listingAddress.formattedAddress,
+            addressComponents: addressComponentsObj,
+            geographic: {
+              lat:
+                listingObj.listingAddress.googleGeocodingData?.geometry
+                  ?.location.lat || null,
+              lng:
+                listingObj.listingAddress.googleGeocodingData?.geometry
+                  ?.location.lng || null,
+            },
+            googlePlaceId:
+              // eslint-disable-next-line camelcase
+              listingObj.listingAddress.googleGeocodingData?.place_id || null,
+            note: listingObj.listingAddress.note || null,
+          },
+          status: listingObj.listingStatus.statusType,
+          createdAt: listingObj.createdAt,
+          updatedAt: listingObj.updatedAt,
+        };
+
+        if (listingObj.userId !== req.user?.id) {
+          // If user isn't creator.
+
+          responseObj.preciseLocation = null;
+        }
+
+        return this.ok(res, responseObj);
+      }
+
+      return this.notFound(res, null);
     } catch (error) {
       return this.fail(res, error);
     }

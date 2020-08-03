024e327c9d4ebb93d2124df9d5b1eb977ecc1d01
Chehan Ratnasiri <chehan.rat@gmail.com>
2020-08-03 20:27:03 +1000

    Added listing rental model.

---
+++ src/models/listing-rental.js
@@ -0,0 +1,46 @@
+const { Model, UUIDV4 } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class ListingRental extends Model {
+    static associate(models) {
+      this.belongsTo(models.ListingEnquiry, {
+        as: 'listingEnquiry',
+        foreignKey: 'enquiryId',
+      });
+    }
+  }
+
+  ListingRental.init(
+    {
+      id: {
+        allowNull: false,
+        defaultValue: UUIDV4,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        unique: true,
+      },
+      rentalStatus: {
+        allowNull: false,
+        defaultValue: 'pending',
+        type: DataTypes.ENUM('pending', 'picked', 'returned', 'cancelled'),
+      },
+      pickedAt: {
+        type: DataTypes.DATE,
+      },
+      returnedAt: {
+        type: DataTypes.DATE,
+      },
+      cancelledAt: {
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      underscored: true,
+      paranoid: true,
+      tableName: 'listing_rentals',
+    }
+  );
+
+  return ListingRental;
+};

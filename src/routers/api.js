29a21e1bf578144d122e7363ad7c36326a6ba503
Chehan Ratnasiri <chehan.rat@gmail.com>
2020-08-21 00:14:35 +1000

    Registered new route.

--- src/routers/api.js
+++ src/routers/api.js
@@ -7,6 +7,7 @@ const usersRouter = require('./v1_0/users');
 const listingsRouter = require('./v1_0/listings');
 const enquiriesRouter = require('./v1_0/enquiries');
 const rentalsRouter = require('./v1_0/rentals');
+const purchaseRouter = require('./v1_0/purchases');
 const chatsRouter = require('./v1_0/chats');
 
 router.use('/v1_0/auth', authRouter);
@@ -14,6 +15,7 @@ router.use('/v1_0/users', usersRouter);
 router.use('/v1_0/listings', listingsRouter);
 router.use('/v1_0/enquiries', enquiriesRouter);
 router.use('/v1_0/rentals', rentalsRouter);
+router.use('/v1_0/purchases', purchaseRouter);
 router.use('/v1_0/chats', chatsRouter);
 
 module.exports = router;

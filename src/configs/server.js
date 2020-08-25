58b75acc74cbb793e3d50ebea391bfb171e16d6c
Chehan Ratnasiri <chehan.rat@gmail.com>
2020-08-26 01:12:06 +1000

    Added b2 envs to config.

--- src/configs/server.js
+++ src/configs/server.js
@@ -6,4 +6,9 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   bcryptSaltRounds: parseInt(process.env.BCRYPT_SALT_ROUNDS || '10', 10),
   googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+  storageProvider: process.env.STORAGE_PROVIDER || 'B2',
+  b2KeyId: process.env.B2_KEY_ID,
+  b2AppKey: process.env.B2_APP_KEY,
+  b2BucketName: process.env.B2_BUCKET_NAME || 'lushcloset',
+  b2BucketEndpoint: process.env.B2_BUCKET_ENDPOINT,
 };

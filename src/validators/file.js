7baf73e291b6791da4fdf3ce32f48f48bf9fde19
Chehan Ratnasiri <chehan.rat@gmail.com>
2020-08-26 21:51:34 +1000

    Validator file for files.

---
+++ src/validators/file.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+
+module.exports = {
+  createFileSchema: {
+    FILES: Joi.object({
+      file: Joi.object({
+        size: Joi.number()
+          .positive()
+          .max(1 * 1024 * 1024),
+        type: Joi.string().valid('image/jpeg'), // Only accepts jpegs for now.
+      }).required(),
+    }),
+
+    BODY: Joi.object({
+      purpose: Joi.string().valid('listing_image', 'user_avatar').required(),
+    }),
+  },
+};

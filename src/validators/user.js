b7f62fabc2e2507d733e55d343498460c036f328
Chehan Ratnasiri <chehan.rat@gmail.com>
2020-09-02 18:18:53 +1000

    Removed unused validators.

--- src/validators/user.js
+++ src/validators/user.js
@@ -6,12 +6,4 @@ module.exports = {
       userId: Joi.string().uuid().required(),
     }),
   },
-  updateUserItemSchema: {
-    PARAMS: Joi.object({
-      userId: Joi.string().uuid().required(),
-    }),
-    BODY: Joi.object({
-      name: Joi.string().max(256),
-    }),
-  },
 };

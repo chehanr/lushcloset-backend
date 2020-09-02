4917a7d122eb0904d946f51731d1aaa3314e924e
Chehan Ratnasiri <chehan.rat@gmail.com>
2020-09-02 18:10:09 +1000

    New router for me.

---
+++ src/routers/v1_0/me.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const authMiddleware = require('../../middlewares/auth');
+const validationMiddleware = require('../../middlewares/validation');
+const meController = require('../../controllers/me');
+const meValidator = require('../../validators/me');
+
+const router = express.Router();
+
+// /                          get, put
+
+router.get(
+  '/',
+  authMiddleware.authRequired,
+  validationMiddleware(meValidator.getMeSchema),
+  async (req, res) => {
+    await meController.getMe(req, res);
+  }
+);
+
+router.put(
+  '/',
+  authMiddleware.authRequired,
+  validationMiddleware(meValidator.updateMeSchema),
+  async (req, res) => {
+    await meController.updateMe(req, res);
+  }
+);
+
+module.exports = router;

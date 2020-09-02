590be46b96fecb199cf288eb6df54b641d31f1e4
Chehan Ratnasiri <chehan.rat@gmail.com>
2020-09-02 18:18:12 +1000

    Refactored get endpoint.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -8,122 +8,37 @@ class UserController extends BaseController {
    * with params `userId`.
    */
   async retrieveUserItem(req, res) {
-    let errorResponseObj;
+    let errorResponseData;
 
     if (req.validated.params?.error) {
-      errorResponseObj = errorResponses.validationParamError;
-      errorResponseObj.extra = req.validated.params.error;
+      errorResponseData = errorResponses.validationParamError;
+      errorResponseData.extra = req.validated.params.error;
 
-      return this.unprocessableEntity(res, errorResponseObj);
+      return this.unprocessableEntity(res, errorResponseData);
     }
 
+    const { userId } = req.validated.params.value;
+
+    let userObj;
+
     try {
-      const userObj = await models.User.findByPk(
-        req.validated.params.value.userId
-      );
-
-      if (userObj) {
-        const responseObj = {
-          id: userObj.id,
-          name: userObj.name,
-          photoUri: userObj.photoUri,
-        };
-
-        return this.ok(res, responseObj);
-      }
-
-      return this.notFound(res, null);
+      userObj = await models.User.findByPk(userId);
     } catch (error) {
       return this.fail(res, error);
     }
-  }
 
-  /**
-   * Update a user \
-   * with params `userId`.
-   */
-  async updateUserItem(req, res) {
-    let errorResponseObj;
-
-    if (req.validated.params?.error) {
-      errorResponseObj = errorResponses.validationParamError;
-      errorResponseObj.extra = req.validated.params.error;
-
-      return this.unprocessableEntity(res, errorResponseObj);
+    if (!userObj) {
+      return this.notFound(res);
     }
 
-    if (req.validated.body?.error) {
-      errorResponseObj = errorResponses.validationBodyError;
-      errorResponseObj.extra = req.validated.body.error;
+    const responseData = {
+      id: userObj.id,
+      name: userObj.name,
+      avatar: {},
+      createdAt: userObj.createdAt,
+    };
 
-      return this.unprocessableEntity(res, errorResponseObj);
-    }
-
-    try {
-      let userObj = await models.User.findByPk(
-        req.validated.params.value.userId
-      );
-
-      if (userObj) {
-        if (userObj.id !== req.user?.id) {
-          return this.unauthorized(res, null);
-        }
-
-        // Update `name`.
-        const userUpdateData = {};
-
-        if (
-          typeof req.validated.body.value.name !== 'undefined' &&
-          req.validated.body.value.name !== userObj.name
-        ) {
-          userUpdateData.name = req.validated.body.value.name;
-        }
-
-        if (Object.keys(userUpdateData).length) {
-          // Only updated if needed.
-          userObj = await models.sequelize.transaction(async (t) => {
-            return userObj.update(userUpdateData, {
-              returning: true,
-              plain: true,
-              transaction: t,
-            });
-          });
-        }
-
-        const responseObj = {
-          id: userObj.id,
-          name: userObj.name,
-          photoUri: userObj.photoUri,
-        };
-
-        return this.ok(res, responseObj);
-      }
-
-      return this.notFound(res, null);
-    } catch (error) {
-      return this.fail(res, error);
-    }
-  }
-
-  /**
-   * Retrieve the authenticated user.
-   */
-  async retrieveAuthenticatedUserItem(req, res) {
-    if (req.user) {
-      const responseObj = {
-        id: req.user.id,
-        email: req.user.email,
-        name: req.user.name,
-        photoUri: req.user.photoUri,
-        createdAt: req.user.createdAt,
-        updatedAt: req.user.updatedAt,
-      };
-
-      return this.ok(res, responseObj);
-    }
-
-    // 404 or 500?
-    return this.notFound(res, null);
+    return this.ok(res, responseData);
   }
 }
 

dcb018920277582684798fda642a9549acd68dd5
Chehan Ratnasiri <chehan.rat@gmail.com>
2020-09-05 16:16:03 +1000

    Added new controller.

--- src/controllers/me.js
+++ src/controllers/me.js
@@ -68,6 +68,89 @@ class MeController extends BaseController {
 
     return this.ok(res, responseData);
   }
+
+  /**
+   * Create an avatar (file) for a user.
+   */
+  async createAvatar(req, res) {
+    let errorResponseData;
+
+    if (req.validated.body?.error) {
+      errorResponseData = errorResponses.validationBodyError;
+      errorResponseData.extra = req.validated.body.error;
+
+      return this.unprocessableEntity(res, errorResponseData);
+    }
+
+    const { fileId } = req.validated.body.value;
+
+    let userObj;
+    let fileObj;
+
+    try {
+      userObj = await models.User.findByPk(req.user.id, {
+        include: [
+          {
+            model: models.UserAvatar,
+            as: 'userAvatar',
+          },
+        ],
+      });
+
+      fileObj = await models.File.findByPk(fileId);
+    } catch (error) {
+      return this.fail(res, error);
+    }
+
+    if (!fileObj) {
+      errorResponseData = errorResponses.fileNotFoundError;
+      return this.notFound(res, errorResponseData);
+    }
+
+    if (fileObj.userId !== userObj.id) {
+      return this.unauthorized(res);
+    }
+
+    if (fileObj.purpose !== 'user_avatar') {
+      errorResponseData = errorResponses.invalidFilePurposeError;
+      return this.badRequest(res, errorResponseData);
+    }
+
+    let userAvatarObj;
+
+    try {
+      await models.sequelize.transaction(async (transaction) => {
+        if (userObj.userAvatar) {
+          // Delete previous avatar.
+          await userObj.userAvatar.destroy({ transaction });
+        }
+
+        userAvatarObj = await models.UserAvatar.create(
+          {
+            userId: userObj.id,
+            fileId,
+          },
+          {
+            transaction,
+          }
+        );
+
+        userAvatarObj.file = await userAvatarObj.getFile({
+          include: [
+            {
+              model: models.FileLink,
+              as: 'fileLinks',
+            },
+          ],
+          transaction,
+        });
+      });
+    } catch (error) {
+      return this.fail(res, error);
+    }
+
+    return this.ok(res);
+  }
 }
 
 module.exports = new MeController();

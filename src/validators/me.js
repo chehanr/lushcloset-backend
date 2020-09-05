c94bc2ca7a8e0d7b0a452e6c092832bc2ced475d
Chehan Ratnasiri <chehan.rat@gmail.com>
2020-09-05 16:17:35 +1000

    New validator.

--- src/validators/me.js
+++ src/validators/me.js
@@ -2,9 +2,16 @@ const Joi = require('joi');
 
 module.exports = {
   getMeSchema: {},
+
   updateMeSchema: {
     BODY: Joi.object({
       name: Joi.string().max(256),
+    }),
+  },
+
+  createAvatarSchema: {
+    BODY: Joi.object({
+      fileId: Joi.string().uuid().required(),
     }),
   },
 };
